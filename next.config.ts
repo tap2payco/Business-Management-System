b1b017f0050e5bc05a621048f4c4c9203fe3755a
tap2payco <tap2payco@gmail.com>
2025-12-05 21:42:50 +0300

    commit changes all

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 
-
 import type { NextConfig } from 'next';
+
 const nextConfig: NextConfig = {
-	reactStrictMode: true,
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
+  reactStrictMode: true,
 };
+
 export default nextConfig;
